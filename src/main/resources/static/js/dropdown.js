762e81dafed1b12c1bc9317b5762e66699ec3bc9
Sander Revenberg <sander@revenberg.info>
2019-12-23 22:42:18 +0100

    .

--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -1,5 +1,9 @@
 function myFunctionBundleDropbtn(p) {
     var btn =  document.getElementById('bundledropbtn');
     btn.innerHTML = document.getElementById(p).innerHTML;
+}
 
+function myFunctionSongDropbtn(p) {
+    var btn =  document.getElementById('songdropbtn');
+    btn.innerHTML = document.getElementById(p).innerHTML;
 }

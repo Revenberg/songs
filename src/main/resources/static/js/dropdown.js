998d2fdf937362865f3e7141e48fffb30dc372da
Sander Revenberg <sander@revenberg.info>
2019-12-23 20:47:15 +0100

    .

--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -1,6 +1,7 @@
 function myFunctionDropbtn(p) {
     var btn =  document.getElementById('dropbtn');
 
+    document.getElementById('dropbtn').innerHTML = document.getElementById(5).innerHTML;
     btn.innerHTML = p;    
 
 }

221f10e4a772a40d0c2b877799cd8da21a2b9b9c
Sander Revenberg <sander@revenberg.info>
2019-12-23 16:11:34 +0100

    .

--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -1,5 +1,5 @@
-function myFunction(p) {
-    var btn =  document.getElementById(p.id);
+function myFunction(buttn, p) {
+    var btn =  document.getElementById(buttn);
 
     btn.innerHTML = p.innerHTML;    
 

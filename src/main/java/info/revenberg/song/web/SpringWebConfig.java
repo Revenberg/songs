852304025d6025e143d38b94a4c55464d4aa0f89
Sander Revenberg <sander@revenberg.info>
2019-12-24 16:21:16 +0100

    .

--- src/main/java/info/revenberg/song/web/SpringWebConfig.java
+++ src/main/java/info/revenberg/song/web/SpringWebConfig.java
@@ -69,9 +69,9 @@ public class SpringWebConfig
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-        registry.addResourceHandler("/images/**").addResourceLocations("/static/images/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("../../images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("../../css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("../../js/");
     }
 
     /*

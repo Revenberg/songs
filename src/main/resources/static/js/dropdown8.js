e7de9e41ccecf5cb9483588f6fdc3953426b8448
Sander Revenberg <sander@revenberg.info>
2019-12-27 17:10:46 +0100

    .

--- src/main/resources/static/js/dropdown8.js
+++ src/main/resources/static/js/dropdown8.js
@@ -13,12 +13,25 @@ function getSongs(item)
     var url = '/songs/' + item.value;
     $("#resultsBlockSongs").load(url);
     var btn =  document.getElementById('bundledropbtn');
-    btn.innerHTML = document.getElementById("bundle_" + item).innerHTML;
+
+    window.alert("getSongs");
+    var e = document.getElementById("dropdown-content-bundle");
+    var value = e.options[e.selectedIndex].value;
+    window.alert(value);
+    window.alert(value.innerHTML);
+
+    btn.innerHTML = value.innerHTML;
 }
 function getVerses(item)
 {
     var url = '/verses/' + item.value;
     $("#resultsBlockVerses").load(url);
     var btn =  document.getElementById('songdropbtn');
-    btn.innerHTML = document.getElementById("song_" + item).innerHTML;
+    window.alert("getVerses");
+    var e = document.getElementById("dropdown-content-song");
+    var value = e.options[e.selectedIndex].value;
+    window.alert(value);
+    window.alert(value.innerHTML);
+
+    btn.innerHTML = value.innerHTML;
 }

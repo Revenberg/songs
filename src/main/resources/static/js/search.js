1d6e821282d6ec5c56c712b929ce4a98882f93b6
Sander Revenberg <sander@revenberg.info>
2019-12-27 10:41:52 +0100

    .

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,6 +1,10 @@
 function retrieveBundles(p) {
     var url = '/bundles/' + p;
     $("#resultsBlock").load(url);
+    var btn =  document.getElementById('bundledropbtn');
+    btn.innerHTML = document.getElementById(p).innerHTML;
+    var drpdwn =  document.getElementById('dropdown-content');
+    drpdwn.style.display = "none";
 }
 
 function retrieveBundles1() {

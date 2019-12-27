30ce17388038dbb951ce4804cb27bd50cfd3f899
Sander Revenberg <sander@revenberg.info>
2019-12-27 17:31:58 +0100

    .

--- src/main/resources/static/js/dropdown8.js
+++ src/main/resources/static/js/dropdown8.js
@@ -1,29 +1,21 @@
 function myFunctionBundleDropbtn(p) {
-    window.alert("myFunctionBundleDropbtn");
-
     var btn =  document.getElementById('bundledropbtn');
     btn.innerHTML = document.getElementById(p).innerHTML;
 }
 
 function myFunctionSongDropbtn(p) {
-    window.alert("myFunctionSongDropbtn");
-
     var btn =  document.getElementById('songdropbtn');
     btn.innerHTML = document.getElementById(p).innerHTML;
 }
 
 function getSongs(item)
 {
-    window.alert("getSongs");
-    
     var url = '/songs/' + item.value;
     $("#resultsBlockSongs").load(url);
  
 }
 function getVerses(item)
 {
-    window.alert("getVerses");
-    
     var url = '/verses/' + item.value;
     $("#resultsBlockVerses").load(url); 
 }
--- src/main/resources/static/js/search8.js
+++ src/main/resources/static/js/search8.js
@@ -1,21 +1,14 @@
 function retrieveSongs(p) {
-    window.alert("retrieveSongs");
-
     var url = '/songs/' + p;
     $("#resultsBlockSongs").load(url);
 
     var btn =  document.getElementById('bundledropbtn');
-    window.alert(p);
     btn.innerHTML = document.getElementById("bundle_" + p).innerHTML;
 }
 
 function retrieveVerses(p) {
-    window.alert("retrieveVerses");
-
     var url = '/verses/' + p;
     $("#resultsBlockVerses").load(url);
-
-    window.alert(p);
 
     var btn =  document.getElementById('songdropbtn');
     btn.innerHTML = document.getElementById("song_" + p).innerHTML;

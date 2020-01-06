b00bcab5dc42c015f25e1cacf0382917b5596d7b
Sander Revenberg <sander@revenberg.info>
2020-01-06 21:16:22 +0100

    .

--- src/main/resources/static/js/checkbox8.js
+++ src/main/resources/static/js/checkbox8.js
@@ -4,5 +4,11 @@ function checkbox(item)
     window.alert(item.value);
     window.alert(item.id);
     window.alert(item.checked);
-
+    if (item.checked) {
+        var btn =  document.getElementById('bundleid');
+        btn.value = btn.value + "+" + item.value;
+    } else {
+        var btn =  document.getElementById('bundleid');
+        btn.value = btn.value + "-" + item.value;
+    }
 }

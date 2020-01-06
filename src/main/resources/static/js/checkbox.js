065085d470e8b232ab5ac433cef4c95d509702a2
Sander Revenberg <sander@revenberg.info>
2020-01-06 20:32:54 +0100

    .

--- src/main/resources/static/js/checkbox.js
+++ src/main/resources/static/js/checkbox.js
@@ -1,9 +1,6 @@
-$(document).ready(function () {
-    $("input[type='checkbox']").click(function (e) {
-        if ($(this).is(':checked')) {
-            window.alert("true");
-        } else {
-            window.alert("false");
-        }
-    });
-})
+
+function checkpoint(this)
+{
+    window.alert("!!!!!!!!!!!!");
+
+}

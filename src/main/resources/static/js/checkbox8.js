9b193e6522949a1a0e512e8a223048448f7eca03
Sander Revenberg <sander@revenberg.info>
2020-01-15 19:53:35 +0100

    .

--- src/main/resources/static/js/checkbox8.js
+++ src/main/resources/static/js/checkbox8.js
@@ -2,24 +2,31 @@
 function checkbox(item)
 {
     if (item.checked) {
-        var btn =  document.getElementById('versesvalue');
+        var btn =  document.getElementById('versesid');
         btn.value = btn.value + " " + item.id.replace("vers-", "");
     } else {
-        var btn =  document.getElementById('versesvalue');
+        var btn =  document.getElementById('versesid');
         btn.value = btn.value.replace( item.id.replace("vers-", ""), "");
     }
 }
 
-// Listen for click on toggle checkbox
-$('#select-all').click(function(event) {   
-    if(this.checked) {
-        // Iterate each checkbox
-        $(':checkbox').each(function() {
-            this.checked = true;                        
-        });
-    } else {
-        $(':checkbox').each(function() {
-            this.checked = false;                       
-        });
-    }
-});
+function getcheckboxes() {
+    var node_list = document.getElementsByTagName('input');
+    var checkboxes = [];
+    for (var i = 0; i < node_list.length; i++) 
+    {
+        var node = node_list[i];
+        if (node.getAttribute('type') == 'checkbox') 
+    {
+            checkboxes.push(node);
+        }
+    } 
+    return checkboxes;
+}
+function toggle(source) {
+  checkboxes = getcheckboxes();
+  for(var i=0, n=checkboxes.length;i<n;i++) 
+  {
+    checkboxes[i].checked = source.checked;
+  }
+}
